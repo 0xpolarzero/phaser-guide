066cc40b4ea2370a14a020d2d5cb39fa6b75ce23
polarzero <0xpolarzero@gmail.com>
2024-03-29 15:24:43 +0100

    Complete tutorial

--- src/game/main.ts
+++ src/game/main.ts
@@ -1,15 +1,22 @@
 import { AUTO, Game } from 'phaser';
-import { Base } from './scenes/base';
+import { Scene } from './scene';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
-  width: 1024,
-  height: 768,
+  width: 800,
+  height: 600,
   parent: 'game-container',
   backgroundColor: '#028af8',
-  scene: [Base],
+  scene: Scene,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 300 },
+      debug: false,
+    },
+  },
 };
 
 const StartGame = (parent: string) => {
