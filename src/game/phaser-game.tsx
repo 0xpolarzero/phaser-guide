089747dbe2ab9cb8d9b17914a63040c5a4047b77
polarzero <0xpolarzero@gmail.com>
2024-03-29 13:10:52 +0100

    Init cleanup

---
+++ src/game/phaser-game.tsx
@@ -0,0 +1,52 @@
+import { forwardRef, useEffect, useLayoutEffect, useRef } from 'react';
+import StartGame from './main';
+import { EventBus } from './event-bus';
+
+export interface IRefPhaserGame {
+  game: Phaser.Game | null;
+  scene: Phaser.Scene | null;
+}
+
+interface IProps {}
+
+export const PhaserGame = forwardRef<IRefPhaserGame, IProps>(
+  function PhaserGame(_, ref) {
+    const game = useRef<Phaser.Game | null>(null!);
+
+    useLayoutEffect(() => {
+      if (game.current === null) {
+        game.current = StartGame('game-container');
+
+        if (typeof ref === 'function') {
+          ref({ game: game.current, scene: null });
+        } else if (ref) {
+          ref.current = { game: game.current, scene: null };
+        }
+      }
+
+      return () => {
+        if (game.current) {
+          game.current.destroy(true);
+          if (game.current !== null) {
+            game.current = null;
+          }
+        }
+      };
+    }, [ref]);
+
+    useEffect(() => {
+      EventBus.on('current-scene-ready', (scene_instance: Phaser.Scene) => {
+        if (typeof ref === 'function') {
+          ref({ game: game.current, scene: scene_instance });
+        } else if (ref) {
+          ref.current = { game: game.current, scene: scene_instance };
+        }
+      });
+      return () => {
+        EventBus.removeListener('current-scene-ready');
+      };
+    }, [ref]);
+
+    return <div id="game-container"></div>;
+  },
+);
